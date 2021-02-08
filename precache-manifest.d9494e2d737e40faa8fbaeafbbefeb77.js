c8cdef98ef475166835cf35df6f42bb78a7562a4
Andrii Haidai <haidai.andriy@gmail.com>
2021-02-08 14:48:56 +0200

    Updates

---
+++ precache-manifest.d9494e2d737e40faa8fbaeafbbefeb77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9e892fb876b54f03c721d588328000",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ff170d5053180105aee9",
+    "url": "./static/css/main.eccd04a7.chunk.css"
+  },
+  {
+    "revision": "4de0300c64b3c3ba3678",
+    "url": "./static/js/2.6461c399.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.6461c399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4c205385fde715a39b9",
+    "url": "./static/js/3.f1f27faf.chunk.js"
+  },
+  {
+    "revision": "ff170d5053180105aee9",
+    "url": "./static/js/main.43429384.chunk.js"
+  },
+  {
+    "revision": "f15a63731e02d91e36a3",
+    "url": "./static/js/runtime-main.36be3d18.js"
+  }
+]);
